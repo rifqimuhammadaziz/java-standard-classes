18b3e1475559a354619c83be4203c86f5025366f
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-12 11:00:33 +0700

    add Math class

---
+++ src/classes/math/MathApp.java
@@ -0,0 +1,13 @@
+package classes.math;
+
+public class MathApp {
+    public static void main(String[] args) {
+        var min = Math.min(120, 150);
+        System.out.println(min);
+
+        var max = Math.max(120, 150);
+        System.out.println(max);
+
+        System.out.println(Math.PI);
+    }
+}
