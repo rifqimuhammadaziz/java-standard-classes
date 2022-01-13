777d6f5e42e3f875a90fe8dbbfd9ca0172433259
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-13 11:36:43 +0700

    add Regular Expression

--- src/classes/regex/RegexApp.java
+++ src/classes/regex/RegexApp.java
@@ -1,2 +1,24 @@
-package classes.regex;public class RegexApp {
+package classes.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexApp {
+    public static void main(String[] args) {
+        String name = "Rifqi Muhammad Aziz";
+
+        /**
+         * Search with pattern
+         * First letter : a-z | A-Z
+         * Between words have 'a' letter
+         * Last letter : a-z | A-Z
+         */
+        Pattern pattern = Pattern.compile("[a-zA-Z]*[a][a-zA-Z]*");
+        Matcher matcher = pattern.matcher(name);
+
+        while (matcher.find()) {
+            String result = matcher.group();
+            System.out.println(result);
+        }
+    }
 }
